9cab8c27b3906458bed351b1e258aeb51af1d511
David Fencsik <david.fencsik@gmail.com>
2023-10-04 10:06:46 -0700

    Initial commit
    Testing PIL/Pillow commands. Draws a simple block-shaped capital "A" using
    ImageDraw.Draw().line() and outputs to a PNG file.

---
+++ hierpy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageDraw
+
+if __name__=="__main__":
+    im = Image.new('RGB', (100, 100), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    draw.line([(20, 90), (20, 6)], (0, 0, 0), 10)
+    draw.line([(20, 10), (80, 10)], (0, 0, 0), 10)
+    draw.line([(80, 6), (80, 90)], (0, 0, 0), 10)
+    draw.line([(20, 50), (80, 50)], (0, 0, 0), 10)
+    im.save('drawing.png')
