bbdb6db73d531248a0b9f6e4a12f3fd365945191
David Fencsik <david.fencsik@gmail.com>
2023-11-08 20:36:32 -0800

    Pull into a function and centralize drawing parameters
    Create a function to handle the drawing, and change the drawing and
    image-creation commands so they use global variables instead of hard-coded
    sizes.

--- hierpy.py
+++ hierpy.py
@@ -2,16 +2,30 @@
 
 from PIL import Image, ImageDraw
 
-if __name__=="__main__":
-    im = Image.new('RGB', (100, 100), (255, 255, 255))
+image_size = (100, 100)
+offset = (20, 10)
+background_color = (255, 255, 255)
+foreground_color = (0, 0, 0)
+thickness = 10
+
+w = image_size[0]
+h = image_size[1]
+ox = offset[0]
+oy = offset[1]
+t = thickness
+
+def DrawA(im):
     draw = ImageDraw.Draw(im)
-    # draw an "A"
     # left
-    draw.rectangle([(15, 5), (25, 90)], (0, 0, 0))
+    draw.rectangle([(ox, oy), (ox + t, h - oy)], foreground_color)
     # top
-    draw.rectangle([(15, 5), (85, 15)], (0, 0, 0))
+    draw.rectangle([(ox, oy), (w - ox, oy + t)], foreground_color)
     # right
-    draw.rectangle([(75, 15), (85, 90)], (0, 0, 0))
+    draw.rectangle([(w - ox - t, oy), (w - ox, h - oy)], foreground_color)
     # middle
-    draw.rectangle([(15, 45), (85, 55)], (0, 0, 0))
+    draw.rectangle([(ox, h / 2 - t / 2), (w - ox, h / 2 + t / 2)], foreground_color)
+
+if __name__=="__main__":
+    im = Image.new('RGB', image_size, background_color)
+    DrawA(im)
     im.save('drawing.png')
